76630b251fccd3124f62e20b9e6a25b4075474d6
qanitasyaf <qanitafiqa@gmail.com>
2025-07-24 11:38:14 +0700

    changes

--- terraform-v2/main.tf
+++ terraform-v2/main.tf
@@ -47,6 +47,8 @@ resource "google_container_cluster" "primary" {
     network  = google_compute_network.custom_network.id
     subnetwork = google_compute_subnetwork.custom_subnet.id
     initial_node_count = 1
+    logging_service = "logging.googleapis.com/kubernetes"
+    monitoring_service = "monitoring.googleapis.com/kubernetes"
     deletion_protection = false
     remove_default_node_pool = true
     }
@@ -56,11 +58,11 @@ resource "google_container_node_pool" "primary_nodes" {
     name = "primary-node-pool"
     cluster = google_container_cluster.primary.name
     location = google_container_cluster.primary.location
-    node_count = 1
+    node_count = 2
 
     node_config {
-        machine_type = "e2-micro"
-        disk_size_gb = 10
+        machine_type = "e2-medium"
+        disk_size_gb = 50
         disk_type = "pd-standard"
         image_type = "UBUNTU_CONTAINERD"
     }
