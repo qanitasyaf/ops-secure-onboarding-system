cc45083d744cad234d5822f3627ce942f38b7e60
qanitasyaf <qanitafiqa@gmail.com>
2025-07-24 11:40:08 +0700

    changes

--- terraform-v2/provider.tf
+++ terraform-v2/provider.tf
@@ -1,14 +1,18 @@
 terraform {
-    required_providers {
-        google = {
-        source  = "hashicorp/google"
-        version = "~> 5.0"
-        }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0" 
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0" 
+    }
+  }
+  required_version = ">= 1.0.0"
 }
 
 provider "google" {
-    project = var.project
-    region  = var.region
-    zone    = var.zone
+  project = var.project
+  region  = var.region
 }
