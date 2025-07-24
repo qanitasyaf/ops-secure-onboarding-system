425135a67a1ed002a201290bdb0d49be5a6d1a33
qanitasyaf <qanitafiqa@gmail.com>
2025-07-24 11:27:09 +0700

    changes

--- terraform-v2/main.tf
+++ terraform-v2/main.tf
@@ -46,14 +46,11 @@ resource "google_container_cluster" "primary" {
     location = var.region
     network  = google_compute_network.custom_network.id
     subnetwork = google_compute_subnetwork.custom_subnet.id
-
+    initial_node_count = 1
     deletion_protection = false
     remove_default_node_pool = true
-
-    lifecycle {
-        ignore_changes = [initial_node_count]
     }
-}
+
 
 resource "google_container_node_pool" "primary_nodes" {
     name = "primary-node-pool"
