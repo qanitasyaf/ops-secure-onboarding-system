c80ade300168776eef53125cde0aa72f022f8c1c
qanitasyaf <qanitafiqa@gmail.com>
2025-07-29 11:16:57 +0700

    node changes

--- terraform-v2/main.tf
+++ terraform-v2/main.tf
@@ -58,7 +58,7 @@ resource "google_container_node_pool" "primary_nodes" {
     name = "primary-node-pool"
     cluster = google_container_cluster.primary.name
     location = google_container_cluster.primary.location
-    node_count = 6
+    node_count = 4
 
     node_config {
         machine_type = "e2-standard-2"
